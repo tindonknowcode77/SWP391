7167ed3d1f10c229621f4122e3cc9196b9f5a861
vietdung <vovietdung20013007@gmail.com>
2025-05-27 16:14:12 +0700

    Update firebase-config.js

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,22 +1,19 @@
-// firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage"; // THÊM để dùng upload ảnh
+import { getStorage } from "firebase/storage";
 
-// ⚠️ Sửa lại storageBucket cho đúng cú pháp!
 const firebaseConfig = {
   apiKey: "AIzaSyDUh25FQ7CzwHVNled2QF3MZfPFImGgk-Y",
   authDomain: "hivtreatment-a7ab9.firebaseapp.com",
   projectId: "hivtreatment-a7ab9",
-  storageBucket: "hivtreatment-a7ab9.appspot.com", 
+  storageBucket: "hivtreatment-a7ab9.appspot.com", // ĐÃ CHỈNH ĐÚNG
   messagingSenderId: "1038700512945",
   appId: "1:1038700512945:web:7a1bd95f51048b28bd8490",
   measurementId: "G-LZ8DGEPVSQ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const storage = getStorage(app); // ✅ cần cho upload ảnh
+const storage = getStorage(app);
 
 export { app, analytics, storage };
